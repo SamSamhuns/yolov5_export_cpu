200f209ce6440a8851ea8598c0ae9e915890e3bb
SamSamhuns <samhunsadamant@gmail.com>
2024-08-07 10:31:06 +0400

    Update to ov 2024 api

--- detect_openvino.py
+++ detect_openvino.py
@@ -5,7 +5,7 @@ from functools import partial
 
 import cv2
 import torch
-from openvino.inference_engine import IECore
+import openvino as ov
 
 from utils.general import DataStreamer
 from utils.detector_utils import save_output, non_max_suppression, preprocess_image
@@ -33,24 +33,24 @@ def parse_arguments(desc: str) -> argparse.Namespace:
 
 
 def get_openvino_core_net_exec(model_xml_path: str, model_bin_path: str, target_device: str = "CPU"):
-    # load IECore object
-    OVIE = IECore()
+    # load openvino Core object
+    core = ov.Core()
 
     # load CPU extensions if availabel
     lib_ext_path = '/opt/intel/openvino/inference_engine/lib/intel64/libcpu_extension.so'
     if 'CPU' in target_device and os.path.exists(lib_ext_path):
         print(f"Loading CPU extensions from {lib_ext_path}")
-        OVIE.add_extension(lib_ext_path, "CPU")
+        core.add_extension(lib_ext_path)
 
     # load openVINO network
-    OVNet = OVIE.read_network(
+    model = core.read_model(
         model=model_xml_path, weights=model_bin_path)
 
     # create executable network
-    OVExec = OVIE.load_network(
-        network=OVNet, device_name=target_device)
+    compiled_model = core.compile_model(
+        model=model, device_name=target_device)
 
-    return OVIE, OVNet, OVExec
+    return core, model, compiled_model
 
 
 def inference(args: argparse.Namespace) -> None:
@@ -58,34 +58,33 @@ def inference(args: argparse.Namespace) -> None:
 
     args: ArgumentParser Namespace
     """
-    print("Running Inference for {}: {}".format(args.media_type, args.input_path))
-    # Load Network and Executable
-    OVIE, OVNet, OVExec = get_openvino_core_net_exec(
+    print(f"Running Inference for {args.media_type}: {args.input_path}")
+    # Load model and executable
+    core, model, compiled_model = get_openvino_core_net_exec(
         args.model_xml, args.model_bin, args.target_device)
 
     # Get Input, Output Information
-    InputLayer = next(iter(OVNet.input_info))
-    OutputLayer = list(OVNet.outputs)[-1]
+    print("Available Devices: ", core.available_devices)
+    print("Input layer names ", model.inputs[0].names)
+    print("Output layer names ", model.outputs[0].names)
+    print("Input Layer: ", model.inputs)
+    print("Output Layer: ", model.outputs)
 
-    print("Available Devices: ", OVIE.available_devices)
-    print("Input Layer: ", InputLayer)
-    print("Output Layer: ", OutputLayer)
-    print("Model Input Shape: ",
-          OVNet.input_info[InputLayer].input_data.shape)
-    print("Model Output Shape: ", OVNet.outputs[OutputLayer].shape)
-
+    output_layer_ir = compiled_model.output(model.outputs[0].names.pop())
     if args.output_dir is not None:
         os.makedirs(args.output_dir, exist_ok=True)
 
     start_time = time.time()
-    _, C, H, W = OVNet.input_info[InputLayer].input_data.shape
+    _, C, H, W = model.inputs[0].shape
     preprocess_func = partial(preprocess_image, in_size=(W, H))
-    data_stream = DataStreamer(args.input_path, args.media_type, preprocess_func)
+    data_stream = DataStreamer(
+        args.input_path, args.media_type, preprocess_func)
 
     for i, (orig_input, model_input) in enumerate(data_stream, start=1):
         # Inference
         start = time.time()
-        results = OVExec.infer(inputs={InputLayer: model_input})
+        # results = compiled_model.infer(inputs={InputLayer: model_input})
+        results = compiled_model([model_input])
         end = time.time()
 
         inf_time = end - start
@@ -99,7 +98,7 @@ def inference(args: argparse.Namespace) -> None:
                     0.6, (0, 125, 255), 1)
 
         # Print Bounding Boxes on Image
-        detections = results[OutputLayer]
+        detections = results[output_layer_ir]
         detections = torch.from_numpy(detections)
         detections = non_max_suppression(
             detections, conf_thres=0.4, iou_thres=0.5, agnostic=False)
@@ -117,6 +116,6 @@ def inference(args: argparse.Namespace) -> None:
 
 
 if __name__ == '__main__':
-    args = parse_arguments(
+    parsed_args = parse_arguments(
         desc="Basic OpenVINO Example for person/object detection")
-    inference(args)
+    inference(parsed_args)
